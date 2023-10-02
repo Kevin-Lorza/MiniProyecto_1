d1d0776b572d1adbca8f84fcc6062dee42eed7b3
Juan DAavid Perez <juandapeva282@gmail.com>
2023-10-02 15:25:23 -0500

    actualizar

--- src/Partido.java
+++ src/Partido.java
@@ -1,3 +1,3 @@
 public enum Partido {
-    Liberal, Conservador, Centro_democratico, Alianza_verde, Partido_de_la_u, Partido_cambio_radical,
+    Liberal, Conservador, Centro_democratico, Alianza_verde, Partido_de_la_u, Partido_cambio_radical
 }
